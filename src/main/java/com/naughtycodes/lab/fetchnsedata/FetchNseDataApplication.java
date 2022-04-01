f049e228f7b7e516071c8de16324cb7c9ecc4444
krish_nan@outlook <krish_nan@outlook.com>
2022-04-02 01:30:18 +0530

    inital commit

---
+++ src/main/java/com/naughtycodes/lab/fetchnsedata/FetchNseDataApplication.java
@@ -0,0 +1,13 @@
+package com.naughtycodes.lab.fetchnsedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchNseDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchNseDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naughtycodes/lab/fetchnsedata/FetchNseDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naughtycodes.lab.fetchnsedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchNseDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
